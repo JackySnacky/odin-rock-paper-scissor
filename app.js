fb220bc5ab31c13622608c4fb8e289a7adbc5cde
Jacky <jacky.chuong14@gmail.com>
2023-03-06 18:44:32 -0600

    Feat: tracks who wins the most

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const RPSARRAY = ["rock", "paper", "scissor"];
+// Tracks who wins
+let userWin = 0;
+let computerWin = 0;
 // Start the game
 game();
+console.log("You win: " + userWin + " times.\nComputer win: " + computerWin);
 
 function getComputerChoice() {
     return RPSARRAY[Math.floor(Math.random() * RPSARRAY.length)];
@@ -12,22 +16,28 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "rock") {
         // Rock beats scissor and lose to paper
         if (computerSelection === "scissor") {
+            userWin++;
             return "You win! Rock beats scissor."
         } else {
+            computerWin++;
             return "You lose! Paper beats Rock."
         }
     } else if (playerSelection === "paper") {
         // Paper beats rock and lose to scissor
         if (computerSelection === "rock") {
+            userWin++;
             return "You win! Paper beats rock."
         } else {
+            computerWin++;
             return "You lose! Scissor beats paper."
         }
     } else if (playerSelection === "scissor") {
         // Scissor beats paper and lose to rock
         if (computerSelection === "paper") {
+            userWin++;
             return "You win! Scissor beats paper"
         } else {
+            computerWin++;
             return "You lose! Rock bears scissor"
         }
     }
